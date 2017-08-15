e3a7918b24f58bbf3b7306628f93209824ed7482
Luan Oliveira <luan.m.oliveira@gmail.com>
2017-08-14 23:43:40 -0300

    Estilo e js do slider

--- slider.js
+++ slider.js
@@ -3,32 +3,87 @@ var script = document.createElement('script');
 script.src = src;
 document.body.appendChild(script);
 
-function X(response){
+function X(response) {
 
-	var recs = response.data.recommendation;
+    var recommendations = response.data.recommendation;
+    var reference = response.data.reference;
 
-	for(var i in recs){                
-		var slider = document.getElementById('slider-vitrine');
-        
-		var div = document.createElement('div');
-		div.setAttribute('class', 'col-xs-12 col-sm-6 col-md-2');
+    buildReference(reference);
+    buildRecommendations(recommendations);
 
-		var img = document.createElement('img');
-		img.setAttribute('src', 'http:' + recs[i].imageName);
+    var next = document.querySelector('.slider-right-control');
+    var prev = document.querySelector('.slider-left-control');
 
-		var h4 = document.createElement('div');
-		h4.setAttribute('class', 'caption');
-				
-		var textNode = document.createTextNode(recs[i].name);
+    next.addEventListener('click', function(ev) {
+        changeSlider(1);
+    });
+    prev.addEventListener('click', function(ev) {
+        changeSlider(-1);
+    });
 
-		h4.appendChild(textNode);
+}
 
-		div.appendChild(img);
-		div.appendChild(h4);
+function changeSlider(op){
+	console.log(op);
+}
 
-		slider.appendChild(div);
-                
-	}
+function buildReference(reference) {
 
-	
+    var ref = document.getElementById('product-ref');
+
+    var div = document.createElement('div');
+    div.setAttribute('class', 'product-slide col-md-12');
+
+    var img = document.createElement('img');
+    img.setAttribute('src', 'http:' + reference.item.imageName);
+
+    var h4 = document.createElement('div');
+    h4.setAttribute('class', 'caption');
+
+    var textNode = document.createTextNode(reference.item.name);
+
+    h4.appendChild(textNode);
+
+    div.appendChild(img);
+    div.appendChild(h4);
+
+    ref.appendChild(div);
+
+}
+
+function buildRecommendations(recommendations) {
+
+    var cont = 1;
+
+    for (var i in recommendations) {
+
+        var slider = document.getElementById('slider-vitrine');
+
+        var div = document.createElement('div');
+
+        if (cont > 3) {
+            div.setAttribute('class', 'product-slide col-md-4 inactive');
+        } else {
+            div.setAttribute('class', 'product-slide col-md-4');
+        }
+
+        var img = document.createElement('img');
+        img.setAttribute('src', 'http:' + recommendations[i].imageName);
+
+        var h4 = document.createElement('div');
+        h4.setAttribute('class', 'caption');
+
+        var textNode = document.createTextNode(recommendations[i].name);
+
+        h4.appendChild(textNode);
+
+        div.appendChild(img);
+        div.appendChild(h4);
+
+        slider.appendChild(div);
+
+        cont++;
+
+    }
+
 }
